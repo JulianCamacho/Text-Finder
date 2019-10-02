2a83f7939c8e94ef3cab1cd6ff55540821ac5e36
Brayan <alfabrayan12@gmail.com>
2019-10-02 10:24:29 -0600

    Configurations fix and defintion of interface elements

--- src/src/Controller.java
+++ src/src/Controller.java
@@ -1,7 +1,33 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
 
+    @FXML
+    Button indexBtn;
+
+    @FXML
+    Button refreshBtn;
+
+    @FXML
+    Button addBtn;
+
+    @FXML
+    Button deleteBtn;
+
+    @FXML
+    Button searchBtn;
+
+    @FXML
+    TextField inputField;
+
+    @FXML
+    VBox textPane;
+
+    @FXML
+    VBox namePane;
 
 
     public Controller(){}
