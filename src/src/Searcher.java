ba427b4b96bd66a611cd7aaad21984b0b02d4754
Brayan <alfabrayan12@gmail.com>
2019-10-03 13:25:10 -0600

    Searcher

--- src/src/Searcher.java
+++ src/src/Searcher.java
@@ -1,5 +1,11 @@
-/*
+
+import org.graalvm.compiler.word.Word;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.annotation.ElementType;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 
 public class Searcher {
@@ -7,8 +13,8 @@ public class Searcher {
     Tree tree;
     Controller controller;
 
-    public Searcher(Tree tree, Controller controller){
-        this.tree=tree;
+    public Searcher(Controller controller){
+        this.tree=Tree.getInstance();
         this.controller=controller;
     }
 
@@ -18,15 +24,52 @@ public class Searcher {
 
             WordOcurrences wordOcurrences= this.searchWord(splitPhrase[0]);
             wordOcurrences=this.getRealOcurrences(wordOcurrences,splitPhrase);
+
             String[] context = this.getContext(wordOcurrences);
-            String[] names = wordOcurrences.getDocuments().toArray(new String[wordOcurrences.getDocuments().size()]);
-            String[] dates = {};
-            String[] sizes = {};
+            String[] names = this.getNames(wordOcurrences.getDocuments());
+
+            String[] dates;
+            try {
+                dates = this.getDates(wordOcurrences.getDocuments());
+            } catch (IOException e) {
+                dates=null;
+            }
+
+            String[] sizes;
+            try {
+                sizes = this.getSizes(wordOcurrences.getDocuments());
+            } catch (IOException e) {
+                sizes = null;
+            }
             controller.updateSearchPane(context, names, dates,sizes);
 
         }else{
-            //Interfaz.show(no input entered)
+            AlertBoxes.displayAlertBox("Error", "No ocurrences were found");
         }
+    }
+
+
+    private String[] getNames(ArrayList<File> documents){
+        String[] names = new String[documents.size()];
+        for (int i = 0; i<documents.size();i++){
+            names[i]=documents.get(i).getName();
+        }
+        return names;
+    }
+    private String[] getSizes(ArrayList<File> documents) throws IOException {
+        String[] sizes = new String[documents.size()];
+        for (int i = 0; i<documents.size();i++){
+            sizes[i]=String.valueOf(documents.get(i).length())+" bytes";
+        }
+        return sizes;
+    }
+
+    private String[] getDates(ArrayList<File> documents) throws IOException {
+        String[] dates = new String[documents.size()];
+        for (int i = 0; i<documents.size();i++){
+            dates[i]= Files.readAttributes(documents.get(i).toPath(), BasicFileAttributes.class).creationTime().toString();
+        }
+        return dates;
     }
 
     private WordOcurrences searchWord(String word){
@@ -35,15 +78,73 @@ public class Searcher {
     }
 
     private WordOcurrences getRealOcurrences(WordOcurrences wordOcurrences, String[] phrase){
+
         int size = wordOcurrences.getDocuments().size();
+        File document;
+        int lineNumber;
+        int linePos;
+        boolean error=false;
+
+        ArrayList<File> finalDocs = new ArrayList<>();
+        ArrayList<Integer> finalLineNumber = new ArrayList<>();
+        ArrayList<Integer>  finalLinePos = new ArrayList<>();
+
         for(int i = 0; i < size; i++){
-           //Updates stuff
+            document = wordOcurrences.getDocuments().get(i);
+            String[][] content =this.getContent(document);
+            lineNumber=wordOcurrences.getLineNumber().get(i);
+            linePos=wordOcurrences.getLinePos().get(i);
+
+            for(int j = 0; j<phrase.length;j++){
+                error= !content[lineNumber+(linePos+j)/content.length][(linePos+j)%content.length].equals(phrase[j]);
+                if (error){
+                    break;
+                }
+            }
+
+            if(!error){
+                finalDocs.add(document);
+                finalLineNumber.add(lineNumber);
+                finalLinePos.add(linePos);
+            }
         }
-        return null;
+
+        WordOcurrences result=new WordOcurrences();
+        result.setDocuments(finalDocs);
+        result.setLineNumber(finalLineNumber);
+        result.setLinePos(finalLinePos);
+        return result;
     }
 
     private String[] getContext(WordOcurrences wordOcurrences){
-        return null;
+        int size = wordOcurrences.getDocuments().size();
+        String[] contexts= new String[size];
+        for(int i=0;i<size;i++){
+            contexts[i]=this.getContext(wordOcurrences.getDocuments().get(i),wordOcurrences.getLineNumber().get(i),wordOcurrences.getLinePos().get(i));
+        }
+        return contexts;
+    }
+
+    private String[][] getContent(File document){
+        int index= controller.getDocuments().indexOf(document);
+        String[][] content = controller.getContents().get(index);
+        return content;
+    }
+
+    private String getContext(File document, int lineNumber, int linePos){
+        String[][] content =this.getContent(document);
+        String context="";
+        try {
+            for (int i = 0; i < 3; i++) {
+                context = content[lineNumber + Math.floorDiv(linePos - i, content.length)][Math.floorMod(linePos - i, content.length)] + context;
+            }
+        }catch(IndexOutOfBoundsException e){}
+        try{
+            for(int i = 0; i<3;i++){
+                context= content[lineNumber+Math.floorDiv(linePos+i,content.length)][Math.floorMod(linePos+i,content.length)]+context;
+            }
+        }catch (IndexOutOfBoundsException e){}
+        return context;
     }
 
     private String[] decomposePhrase(String phrase){
@@ -58,4 +159,3 @@ public class Searcher {
         return split;
     }
 }
-*/
